6948da655ae886dc25013602f46ed7168923aab7
Alba Giraldo <agiraldomolina@gmail.com>
2023-12-26 07:17:47 -0500

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true });
+
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+  };
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
