606e6865eb0a590f21c7ad1f09a10ee235dd0872
Alba Giraldo <agiraldomolina@gmail.com>
2023-12-27 17:59:27 -0500

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,16 +1,22 @@
 
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { FaEye, FaEyeSlash } from'react-icons/fa'
 import {HiMail} from'react-icons/hi'
+import{
+  signInStart,
+  signInSuccess,
+  signInFailure
+} from '../redux/user/userSlice'
 
 export default function SignIn() {
   const navigate = useNavigate();
-  const [ errorMessage, setErrorMessage ] = useState(null);
-  const [ loading, setLoading ] = useState(false);
+  const dispatch = useDispatch();
   const [ formData, setFormData ] = useState({});
   const [ showPassword, setShowPassword ] = useState(true);
+  const  {loading, error: errorMessage} = useSelector(state => state.user)
 
   //const PasswordIcon = showPassword? <FaEyeSlash /> : <FaEye />;
 
@@ -28,8 +34,7 @@ export default function SignIn() {
       return setErrorMessage('Please fill out all the fields');
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart())
       // we need to create a proxy which is specfyed in the vite.config.js file n order this fetch is called
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
@@ -41,14 +46,14 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setLoading(false);
-        return setErrorMessage('Invalid information');
+        dispatch(signInFailure(data.message));
       }  
-      setLoading(false);
-      if (res.ok) navigate('/');       
+
+      if (res.ok) {
+        dispatch(signInSuccess(data))
+        navigate('/')}       
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   }
   return (
