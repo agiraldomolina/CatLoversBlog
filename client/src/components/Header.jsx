f52c4c97eab6a7f8c48858484880ae9808a2c62d
Alba Giraldo <agiraldomolina@gmail.com>
2023-12-31 12:15:44 -0500

    complete sidebar of the dashboard

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,7 +34,7 @@ export default function Header() {
         </Button>
         <div className="flex gap-2 md:order-2">
             <Button  
-                lassName='w-12 h-10 hidden sm:inline' 
+                className='w-12 h-10 hidden sm:inline' 
                 color='gray' 
                 pill
                 onClick={()=>dispatch(changeTheme())}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import DashSidebar from "../components/DashSidebar"
+import DashProfile from "../components/DashProfile"
 
-import React from 'react'
 
 export default function Dashboard() {
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if (tabFromUrl){
+      setTab(tabFromUrl)
+    }
+  }, [location.search])
   return (
-    <div>Dashboard</div>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* SideBar */}
+      <div className="md:w-56">
+        <DashSidebar/>
+      </div>
+      {/* Profile... */}
+      <div className="div">
+        {tab === 'profile' && <DashProfile/> }
+      </div>
+    </div>
   )
 }
